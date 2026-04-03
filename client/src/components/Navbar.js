fbe615dacf2187f7511b01a758758ece3dbcce28
Makenna <makennaiklehnert@gmail.com>
2026-04-02 22:56:24 -0500

    fixed issue with navbar centering

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,8 +11,8 @@ export default async function Navbar() {
     };
 
     return (
-        <nav className="border-b bg-teal-700 h-16">
-            <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
+        <nav className="border-b bg-teal-700 h-15">
+            <div className="max-w-7xl mx-auto px-4 h-full flex justify-between items-center">
                 <Link href="/" className="text-xl font-bold text-white hover:text-teal-100">
                     JobTracker
                 </Link>
